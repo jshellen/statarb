b7471060e1d8927396adc1d55ab8ecab13f78f9c
Juha-Samuli Hellen <juha-samuli.hellen@taaleri.com>
2020-04-09 16:16:19 +0300

    rls test added

--- ou_seq_ols.py
+++ ou_seq_ols.py
@@ -6,10 +6,6 @@ from src.simulation.ornstein_uhlenbeck import (
     simulate_one_ornstein_uhlenbeck_path
 )
 
-from src.estimation.rls import (
-    RLSFilter2,
-    system_identification_setup
-)
 
 
 def RLS(x, d, y, n, mu, s):
@@ -30,73 +26,42 @@ def RLS(x, d, y, n, mu, s):
     return a
 
 
-def test1():
-
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import statsmodels.api as sm
-
-    np.random.seed(5)
-
-    n_steps = 500
-    dt = 1.0 / n_steps
-    x = np.zeros((n_steps, 1))
-    for i in range(2, n_steps):
-        x[i] = 0.01 * dt + 0.99 * x[i - 1] + 0.25 * np.sqrt(dt) * np.random.normal(0, 1)
-
-    x_ = sm.add_constant(np.roll(x, 1)[1:])
-    y_ = x[1:]
-
-    b = RLS(x_, 2, y_, len(x_), 0.94, 20)
-
-    fig, ax = plt.subplots(figsize=(8, 4))
-    plt.plot(b, label='RLS', color='red')
-    plt.suptitle('Recursive Least Squares')
-    plt.legend()
-    plt.show()
-
-
 def main():
 
     import numpy as np
     import matplotlib.pyplot as plt
     import statsmodels.api as sm
 
-    """
-    Simulate Ornstein-Uhlenbeck process
-    """
-    #n_step = 1500
-    #dt = 1.0/n_step
-    #x = simulate_one_ornstein_uhlenbeck_path(0, 5.5, 0, 0.14, dt, n_step)
-    n_steps = 500
-    dt = 1.0 / n_steps
+    n_steps = 1500
+    dt = 1.0 / 250.0
     x = np.zeros((n_steps, 1))
-    for i in range(2, n_steps):
-        x[i] = 0.01 * dt + 0.5 * x[i - 1] + 0.25 * np.sqrt(dt) * np.random.normal(0, 1)
+    for i in range(1, n_steps):
+        x[i] = 0.1 + 0.75 * x[i - 1] + 0.05 * np.random.normal(0, 1)
 
-    """
-    Parameter estimation
-    """
     x_ = sm.add_constant(np.roll(x, 1)[1:])
     y_ = x[1:]
 
     # Padasip filter
-    F_1 = padasip.filters.FilterRLS(2, mu=0.997, eps=1e-2, w="zeros")
-    y, e, w_hat_1 = F_1.run(y_, x_)
-
-    # Feedforward implementation
-    F_2 = RLSFilter2(p=1, lmbda=0.997, delta=1e-2)
-    w_hat_2 = np.array([F_2.ff_fb(x_i[0]) for x_i in x])
+    w_0 = np.array([1, 0.1])
+    F_1 = padasip.filters.FilterRLS(2, mu=0.997, eps=0.999, w_0=w_0.reshape(-1, 1))
+    w_1 = np.zeros((len(y_), 2))
+    w_1[0, :] = w_0
+    for i in range(1, len(y_)):
+        F_1.adapt(y_[i][0], x_[i])
+        w_1[i, :] = F_1.w.flatten()
 
     # Naive implementation
-    w_hat_3 = RLS(x_, 2, y_, len(x_), 0.997, 10)
+    w_3 = RLS(x_, 2, y_, len(x_), 0.997, 10)
 
-    fig, ax = plt.subplots(figsize=(8, 4))
-    plt.title('Time-Dependent ARMA RLSE')
-    ax.plot(w_hat_1, color='blue', ls='-', lw=2)
-    ax.plot(w_hat_2, color='green', ls='-', lw=2)
-    ax.plot(w_hat_3, color='red', ls='-', lw=2)
-    ax.set_ylabel(r'$\beta(t)$', fontsize=15)
+    fig, ax = plt.subplots(1, 3, figsize=(8, 3))
+    ax[0].plot(w_1[10:, 0], color='blue', ls='-', lw=2)
+    ax[0].plot(w_3[10:, 0], color='red', ls='-', lw=2)
+
+    ax[1].plot(w_1[10:, 1], color='blue', ls='-', lw=2)
+    ax[1].plot(w_3[10:, 1], color='red', ls='-', lw=2)
+
+    ax[2].plot(np.array([v[0] for k,v in F_1.Re.items()])[-750:], color='red', ls='-', lw=2)
+
     plt.show()
 
 

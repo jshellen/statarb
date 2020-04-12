adb22bfb5f0e9f75925e28d7b59550398917797d
Juha-Samuli Hellen <juha-samuli.hellen@taaleri.com>
2020-04-12 23:51:28 +0300

    refactoring

--- ou_seq_ols.py
+++ ou_seq_ols.py
@@ -6,7 +6,43 @@ from src.simulation.ornstein_uhlenbeck import (
     simulate_one_ornstein_uhlenbeck_path
 )
 
+class KalmanFilter(object):
 
+    def __init__(self, dt, u, std_acc, std_meas):
+        self.dt = dt
+        self.u = u
+        self.std_acc = std_acc
+        self.A = np.array([[1, self.dt],
+                           [0,       1]
+                           ])
+
+        self.B = np.array([[(self.dt**2)/2], [self.dt]])
+        self.H = np.array([[1, 0]])
+        self.Q = np.array([[(self.dt**4)/4, (self.dt**3)/2],
+                            [(self.dt**3)/2, self.dt**2]]) * self.std_acc**2
+        self.R = std_meas**2
+        self.P = np.eye(self.A.shape[1])
+        self.x = np.array([[0], [0]])
+
+    def predict(self):
+        # Ref :Eq.(9) and Eq.(10)
+        # Update time state
+        self.x = np.dot(self.A, self.x) + np.dot(self.B, self.u)
+        # Calculate error covariance
+        # P= A*P*A' + Q
+        self.P = np.dot(np.dot(self.A, self.P), self.A.T) + self.Q
+        return self.x
+
+    def update(self, z):
+        # Ref :Eq.(11) , Eq.(11) and Eq.(13)
+        # S = H*P*H'+R
+        S = np.dot(self.H, np.dot(self.P, self.H.T)) + self.R
+        # Calculate the Kalman Gain
+        # K = P * H'* inv(H*P*H'+R)
+        K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))  # Eq.(11)
+        self.x = np.round(self.x + np.dot(K, (z - np.dot(self.H, self.x))))  # Eq.(12)
+        I = np.eye(self.H.shape[1])
+        self.P = (I - (K * self.H)) * self.P  # Eq.(13)
 
 def RLS(x, d, y, n, mu, s):
 
@@ -43,12 +79,21 @@ def main():
 
     # Padasip filter
     w_0 = np.array([1, 0.1])
-    F_1 = padasip.filters.FilterRLS(2, mu=0.997, eps=0.999, w_0=w_0.reshape(-1, 1))
+    F_1 = padasip.filters.FilterRLS(2, mu=0.997, eps=0.99999, w_0=w_0.reshape(-1, 1))
     w_1 = np.zeros((len(y_), 2))
     w_1[0, :] = w_0
     for i in range(1, len(y_)):
-        F_1.adapt(y_[i][0], x_[i])
+        F_1.update(y_[i][0], x_[i])
         w_1[i, :] = F_1.w.flatten()
+
+    u = 0
+    std_acc = 5.00  # we assume that the standard deviation of the acceleration is 0.25 (m/s^2)
+    std_meas = 0.0001  # and standard deviation of the measurement is 1.2 (m)
+    kf = KalmanFilter(dt, u, std_acc, std_meas)
+    predictions = []
+    for i in range(0, len(w_1)):
+        predictions.append(kf.predict()[0])
+        kf.update(w_1[i, 0])
 
     # Naive implementation
     w_3 = RLS(x_, 2, y_, len(x_), 0.997, 10)
@@ -59,9 +104,7 @@ def main():
 
     ax[1].plot(w_1[10:, 1], color='blue', ls='-', lw=2)
     ax[1].plot(w_3[10:, 1], color='red', ls='-', lw=2)
-
-    ax[2].plot(np.array([v[0] for k,v in F_1.Re.items()])[-750:], color='red', ls='-', lw=2)
-
+    ax[2].plot(predictions, color='red', ls='-', lw=2)
     plt.show()
 
 
